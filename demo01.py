b16cb23e2deef9427c5a31d9965f2205f6993407
zengxianfa <your@email.com>
2020-03-25 00:38:07 +0800

    第一次向Github 传送代码

---
+++ demo01.py
@@ -0,0 +1,68 @@
+'''print("hello world") #字符串
+print("我真帅") #字符串
+print(2333) #整数
+print(23.23) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典'''
+
+
+# print(('哈哈哈'+'嘻嘻嘻')*100)
+# print(((1+2)*2)*600)
+# print(1+2==4)
+
+'''
+a ="张三"
+b ="李四"
+print(a,b)'''
+
+# a =int(input("请输入:"))
+# b =int(input("请输入:"))
+# print("输出int：",a+b)
+
+
+#数据格式的转换
+
+
+
+# c =type("曾宪法")
+# print(c)
+
+'''print (type("曾")) #字符串 str
+print (type(2333)) #整数 int
+print (type(23.23)) #小数 float
+print (type(True)) #布尔值 bool
+print (type(()))   #元组 tuple
+print (type([]))   #数组 list
+print (type({}))   #字典 dict'''
+
+# a = bool("2333")
+# print (type(a))
+
+
+
+
+
+# a =float (input("请输入："))
+# b =float (input("请输入："))
+# print("输出上面的input获取的内容:",a+b)
+# print(type(a+b))
+
+
+
+
+
+# a ='sdfjasjl;asdjkflasjlfdjas'
+# print(len(a))
+
+
+# a = input('')
+# b = input('')
+# print(len(a+b))
+
+
+
+a =input("请输入：")
+b =input("请输入：")
+print("两段字段的长度是",len(a+b))
