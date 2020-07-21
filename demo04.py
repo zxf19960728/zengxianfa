d151fbc3a1bf187a163cb13adb77c6183b13ee61
xianfa <583923939@qq.com>
2020-07-21 22:20:26 +0800

    123

--- demo04.py
+++ demo04.py
@@ -53,19 +53,21 @@
 #     print("+1",a)
 #     a =a+1
 
-high={}
-low={}
+# high={}
+# low={}
 studentlist = ["张三","李四","王五","小六","高琪","兰南巴","吴勇","狗十","王麻子","刘峰"]
 a =0
 while a< len(studentlist):
-    chengji =int(input("请输入"+studentlist[a]+"成绩:"))
-    if chengji>=60:
-        high[studentlist[a]]=chengji
-    else:
-        low[studentlist[a]] =chengji
-    a =a+1
-print("大于60的：",high)
-print("小于60的：",low)
+    print(a)
+    a =a +1
+#     chengji =int(input("请输入"+studentlist[a]+"成绩:"))
+#     if chengji>=60:
+#         high[studentlist[a]]=chengji
+#     else:
+#         low[studentlist[a]] =chengji
+#     a =a+1
+# print("大于60的：",high)
+# print("小于60的：",low)
 
 
 # a =["张三","李四","王五","小六","高琪","兰南巴","吴勇","狗十","王麻子","刘峰"]
@@ -183,3 +185,6 @@ print("小于60的：",low)
 # except:
 #     print("写错了")
     
+
+
+
